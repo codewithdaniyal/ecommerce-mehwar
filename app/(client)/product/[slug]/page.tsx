aaf8000564b7d0e3ea4f83b3c89d3379486ddf3a
Daniyal Ahmed <datariq0311@gmail.com>
2024-12-28 02:32:14 +0500

    page.tsx

--- app/(client)/product/[slug]/page.tsx
+++ app/(client)/product/[slug]/page.tsx
@@ -3,7 +3,11 @@ import { getProductBySlug } from "@/sanity/helpers";
 import Container from "@/components/Container";
 import ProductDetails from "@/components/ProductDetails";
 
-export default async function ProductPage({ params }: { params: { slug: string } }) {
+interface Params {
+  slug: string;
+}
+
+export default async function ProductPage({ params }: { params: Params }) {
   const product = await getProductBySlug(params.slug);
 
   if (!product) {
