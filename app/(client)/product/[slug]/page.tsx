56022a52803dc23cd91b1fea8ff630c6450f7a92
Daniyal Ahmed <datariq0311@gmail.com>
2024-12-28 14:30:32 +0500

    page.tsx

--- app/(client)/product/[slug]/page.tsx
+++ app/(client)/product/[slug]/page.tsx
@@ -3,7 +3,14 @@ import { getProductBySlug } from "@/sanity/helpers";
 import Container from "@/components/Container";
 import ProductDetails from "@/components/ProductDetails";
 
-export default async function ProductPage({ params }: { params: { slug: string } }) {
+// Define the expected type for PageProps
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default async function ProductPage({ params }: PageProps) {
   const product = await getProductBySlug(params.slug);
 
   if (!product) {
